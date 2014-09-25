e85ba268b73c9d901eda8d732468878bfbee1438
Jamesxu182 <james@James-MacBook-Air.local>
2014-09-25 22:00:00 +0800

    界面的优化，并且创建了缓存机制

--- dist/MyWeather.app/Contents/Resources/MyWeather.py
+++ dist/MyWeather.app/Contents/Resources/MyWeather.py
@@ -5,6 +5,10 @@ import urllib2
 import BeautifulSoup
 import Tkinter
 import sys
+import time
+import os.path
+
+weathers = []
 
 class Weather():
   def __init__(self, day, date, weather, maxdegree, mindegree):
@@ -15,6 +19,30 @@ class Weather():
     self.mindegree = str(mindegree)
     self.message = self.day + '\t' + self.date + '日\t' + self.weather + (25 - len(self.weather)) * ' ' + self.mindegree + '度 ~ ' + self.maxdegree + '度'
 
+def checkUpdate():
+  nowday = time.localtime().tm_mday
+
+  if os.path.isfile('data.txt'):
+    file = open('data.txt')
+    for i in range(6):
+      day = file.readline().strip('\n').strip()
+      date = file.readline().strip('\n').strip()
+      weather = file.readline().strip('\n').strip()
+      maxdegree = file.readline().strip('\n').strip()
+      mindegree = file.readline().strip('\n').strip()
+      global weathers
+      weathers.append(Weather(day, date, weather, maxdegree, mindegree))
+
+    if int(weathers[0].date) != nowday:
+      weathers.clear()
+      weathers = getWeathers()
+
+  else:
+    weathers = getWeathers()
+
+  #for wea in weathers:
+    #print wea.message
+
 def getWeathers():
 
   day = []
@@ -22,7 +50,6 @@ def getWeathers():
   weather = []
   maxdegree = []
   mindegree = []
-  weathers = []
 
   url = 'http://www.weather.com.cn/weather1d/101220101.shtml'
   try:
@@ -60,17 +87,18 @@ def getWeathers():
 
   for dayname, datename, weathername, maxdegreename, mindegreename in zip(day, date, weather, maxdegree, mindegree):
     #print dayname, datename, weathername,  mindegreename, '度 ~', maxdegreename, '度'
+    global weathers
     weathers.append(Weather(dayname, datename, weathername, maxdegreename, mindegreename))
 
   return weathers
 
-if __name__ == '__main__':
+def GUI():
 
-  weathers = getWeathers()
+  checkUpdate()
 
   top = Tkinter.Tk(screenName = 'Weather', className = 'Weather')
-  today = Tkinter.Frame()
-  feture = Tkinter.Frame()
+  today = Tkinter.LabelFrame(top, text = 'Today Weather of Hefei')
+  feture = Tkinter.LabelFrame(top, text = 'Feture Weatehr of Hefei')
 
   Tkinter.Label(today, text = weathers[0].message, bg = 'black', fg = 'red', justify = 'left', anchor = 'w').pack(fill=Tkinter.X)
   today.pack(fill=Tkinter.X)
@@ -79,4 +107,15 @@ if __name__ == '__main__':
     Tkinter.Label(feture, text = wea.message, justify = 'left', anchor = 'w').pack(fill=Tkinter.X)
     feture.pack(fill=Tkinter.X)
 
+  file = open('data.txt', 'w')
+  for wea in weathers:
+    file.write(wea.day + '\n')
+    file.write(wea.date + '\n')
+    file.write(wea.weather + '\n')
+    file.write(wea.maxdegree + '\n')
+    file.write(wea.mindegree + '\n')
+
   top.mainloop()
+
+if __name__ == '__main__':
+  GUI()
