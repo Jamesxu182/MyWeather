2c93013829b17b8e990e4e5caa8b3671c5090e42
Jamesxu182 <james@James-MacBook-Air.local>
2014-11-30 23:08:07 +0800

    对GUI进行了进一步的优化
    设计了全新的Today框架，并且加入了Yahoo的产权图标，加入了天气图标。

--- GUI.py
+++ GUI.py
@@ -4,17 +4,22 @@
 from Tkinter import *
 from Weathers import *
 import urllib2
-import socket
-from SimpleDialog import *
+from threading import Thread
+import tkMessageBox
+from sys import exit
+
+class MyThread(Thread):
+    def run(self):
+        tkMessageBox.showinfo("Error", "The City Is Not Exist!")
 
 class Top(Frame):
-  def __init__(self, master=None, content=None, chart=None):
+  def __init__(self, master=None, content=None, chart=None, today=None):
     Frame.__init__(self, master)
     self.content = content
     self.master = master
     self.chart = chart
+    self.today = today
     self.createWidget()
-
 
   def createWidget(self):
     self.e = StringVar()
@@ -28,9 +33,9 @@ class Top(Frame):
     #create the Button widget
     self.submitbutton.bind("<Return>", self.submitcity)
     #bind the Button namely submit with Enter Key in keyboard
-    self.entry.pack(side="left")
+    self.entry.pack(side='left')
     self.submitbutton.pack(side="right")
-    self.top.pack()
+    self.top.pack(fill=X)
     #place the widgets on frame namely Top
 
   #define the function namely submit and it is the activity of button namely submit
@@ -39,18 +44,37 @@ class Top(Frame):
     #backup the cityname as lastcityname
     cityname = self.entry.get().capitalize()
     #make cityname as a same formate
-    try:
-      self.content.updateWeathers(cityname)
+    if self.content.updateWeathers(cityname) == 1:
+      MyThread().start()
+      cityname = lastcityname
+      self.e.set(lastcityname)
+    else:
       self.chart.updateLineChart()
-    except UnboundLocalError:
-      self.content.weathers.city = lastcityname
-      self.content.weathers.saveWeathers()
-      SimpleDialog(self.master, text = 'The city is not exist!', buttons = ['OK'], default = 0)
-    except AttributeError:
-      self.content.weathers.city = lastcityname
-      self.content.weathers.saveWeathers()
-      SimpleDialog(self.master, text = 'The city is not exist!', buttons = ['OK'], default = 0)
+      self.today.updateToday()
 
+class Today(Frame):
+  def __init__(self, master=None, content=None):
+      Frame.__init__(self, master)
+      self.content = content
+      self.createWidget()
+
+  def createWidget(self):
+      self.today = LabelFrame(self, text='today')
+      self.img = PhotoImage(file='Today.gif')
+      self.canvas = Canvas(self.today, height=90)
+      self.item1 = self.canvas.create_image(150, 50, image=self.img)
+      self.item2 = self.canvas.create_text(20, 10, text=self.content.weathers.day[0])
+      self.item3 = self.canvas.create_text(80, 10, text=self.content.weathers.date[0])
+      self.item4 = self.canvas.create_text(150, 80, text=self.content.weathers[0].mindegree + 'C ~ ' + self.content.weathers[0].maxdegree + 'C')
+      self.canvas.pack(fill=X)
+      self.today.pack(fill=X)
+
+  def updateToday(self):
+      self.img = PhotoImage(file='Today.gif')
+      self.canvas.itemconfigure(self.item1, image=self.img)
+      self.canvas.itemconfigure(self.item2, text=self.content.weathers.day[0])
+      self.canvas.itemconfigure(self.item3, text=self.content.weathers.date[0])
+      self.canvas.itemconfigure(self.item4, text=self.content.weathers[0].mindegree + 'C ~ ' + self.content.weathers[0].maxdegree + 'C')
 
 class Content(Frame):
   def __init__(self, master=None):
@@ -63,44 +87,49 @@ class Content(Frame):
 
   def createWidget(self):
     self.labels = [];
-    self.today = LabelFrame(self, text = 'Today Weather', padx = 5, pady = 5)
+    #self.today = LabelFrame(self, text = 'Today Weather', padx = 5, pady = 5)
     self.feture = LabelFrame(self, text = 'Feture Weather', padx = 5, pady = 5)
 
-    self.labels.append(Label(self.today, text = self.weathers[0].message, justify = 'left', anchor = 'w', fg = 'red'))
-    self.labels[0].pack(fill=X)
+    #self.label.pack()
 
-    for i in range(1, 6):
-      self.labels.append(Label(self.feture, text = self.weathers[i].message, justify = 'left', anchor = 'w'))
+    self.labels.append(Label(self, justify = 'left', anchor = 'w', fg = 'red'))
+    #self.labels[0].pack(fill=X)
+
+    for i in range(1, 5):
+      self.labels.append(Label(self.feture, justify = 'left', anchor = 'w'))
       self.labels[i].pack(fill=X)
 
-    self.today.pack()
-    self.feture.pack()
+    #self.today.pack(fill=X)
+    self.feture.pack(fill=X)
 
   def initWeathers(self):
     try:
       self.weathers.setURL()
       self.weathers.setWeathersFromInternet()
     except urllib2.URLError:
-      SimpleDialog(self.master, text = 'Please check connect!', buttons = ['OK'], default = 0)
-    except socket.timeout:
-      SimpleDialog(self.master, text = 'Please check connect!', buttons = ['OK'], default = 0)
+      tkMessageBox.showinfo("Error", "Please check connect!")
+      exit(0)
 
     for i, wea in enumerate(self.weathers, start=0):
       self.labels[i]['text'] = wea.message
-    #fill in message in the text of labels
+      #fill in message in the text of labels
 
   def updateWeathers(self, cityname):
       self.weathers.setCity(cityname)
       self.weathers.setURL()
       try:
-        self.weathers.setWeathersFromInternet()
-      except urllib2.URLError:
-        SimpleDialog(self.master, text = 'Please check connect!', buttons = ['OK'], default = 0)
 
-      for i, wea in enumerate(self.weathers[0:6], start=0):
+        if self.weathers.setWeathersFromInternet() == 1:
+          return 1
+      except urllib2.URLError:
+        tkMessageBox.showinfo("Error", "Please check connect!")
+        exit(0)
+
+      for i, wea in enumerate(self.weathers, start=0):
         self.labels[i]['text'] = wea.message
 
       self.weathers.saveWeathers()
+      return 0
 
 class LineChart(Frame):
   def __init__(self, master=None, content=None):
@@ -113,8 +142,13 @@ class LineChart(Frame):
   def createWidget(self):
     self.chartframe = LabelFrame(self, text = 'LineChart', padx = 5, pady = 5);
     self.chart = Canvas(self.chartframe, height=200)
-    self.chartframe.pack()
+    self.chartframe.pack(fill=X)
     self.chart.pack()
+    img = PhotoImage(file='./icon/purple_retina.gif')
+    self.label = Label(self)
+    self.label.configure(image=img)
+    self.label.image = img
+    self.label.pack(side='right')
 
   def initChart(self):
     self.chart.create_line(20, 20, 20, 180, fill='black')
@@ -127,47 +161,40 @@ class LineChart(Frame):
     self.chart.create_line(265, 165, 270, 170, fill='black');
     self.chart.create_line(265, 175, 270, 170, fill='black');
 
-    for i in range(0, 6):
-      self.chart.create_line(40 * i + 40, 170, 40 * i + 40, 165, fill='black')
+    for i in range(0, 5):
+      self.chart.create_line(40 * i + 60, 170, 40 * i + 60, 165, fill='black')
 
   def drawLineChart(self):
       self.pointmax = []
       self.pointmin = []
-      try:
-        self.minmin = sorted(self.content.weathers.mindegree)[0]
-        self.minmax = sorted(self.content.weathers.maxdegree)[0]
-      except IndexError:
-          SimpleDialog(self.master, text = 'Please check connect!', buttons = ['OK'], default = 0)
+      self.minmin = sorted(self.content.weathers.mindegree)[0]
+      self.maxmax = sorted(self.content.weathers.maxdegree)[len(self.content.weathers.maxdegree) - 1]
+      self.gap = 150 / (self.maxmax - self.minmin) - 1
 
       for i, d in enumerate(self.content.weathers.maxdegree[0:6]):
-          self.pointmax.append((170 - ((d - self.minmin) * 10) - 10, 40 + i * 40))
+          self.pointmax.append((170 - ((d - self.minmin) * self.gap) - 10, 60 + i * 40))
 
       for i, d in enumerate(self.content.weathers.mindegree[0:6]):
-          self.pointmin.append((170 - ((d - self.minmin) * 10) - 10, 40 + i * 40))
+          self.pointmin.append((170 - ((d - self.minmin) * self.gap) - 10, 60 + i * 40))
 
       for i, wea in enumerate(self.content.weathers, start=0):
-          self.chart.create_text(40 * i + 40, 175, text=wea.date)
+          self.chart.create_text(40 * i + 60, 175, text=wea.date.split()[0])
 
       for i in range(1, len(self.pointmax)):
           self.chart.create_line(20, self.pointmax[i - 1][0], 25, self.pointmax[i - 1][0], fill='red')
-          #self.chart.create_text(25, self.pointmax[i - 1][0], text=self.content.weathers[i].maxdegree, fill='red')
+          self.chart.create_text(30, self.pointmax[i - 1][0], text=self.content.weathers[i - 1].maxdegree, fill='red')
           self.chart.create_line(self.pointmax[i - 1][1], self.pointmax[i - 1][0], self.pointmax[i][1], self.pointmax[i][0], fill='red')
-      try:
-          self.chart.create_line(20, self.pointmax[len(self.pointmax) - 1][0], 25, self.pointmax[len(self.pointmax) - 1][0], fill='red')
-      except:
-          SimpleDialog(self.master, text = 'Please check connect!', buttons = ['OK'], default = 0)
 
-      #self.chart.create_text(25, self.pointmax[len(self.pointmax) - 1][0], text=self.content.weathers[i].maxdegree, fill='blue');
+      self.chart.create_line(20, self.pointmax[len(self.pointmax) - 1][0], 25, self.pointmax[len(self.pointmax) - 1][0], fill='red')
+      self.chart.create_text(30, self.pointmax[len(self.pointmax) - 1][0], text=self.content.weathers[len(self.pointmax) - 1].maxdegree, fill='red');
 
       for i in range(1, len(self.pointmin)):
           self.chart.create_line(15, self.pointmin[i - 1][0], 20, self.pointmin[i - 1][0], fill='blue')
-          #self.chart.create_text(15, self.pointmin[i - 1][0], text=self.content.weathers[i].mindegree, fill='red');
+          self.chart.create_text(10, self.pointmin[i - 1][0], text=self.content.weathers[i - 1].mindegree, fill='blue');
           self.chart.create_line(self.pointmin[i - 1][1], self.pointmin[i - 1][0], self.pointmin[i][1], self.pointmin[i][0], fill='blue')
-      try:
-          self.chart.create_line(15, self.pointmin[len(self.pointmin) - 1][0], 20, self.pointmin[len(self.pointmin) - 1][0], fill='blue')
-      except:
-          SimpleDialog(self.master, text = 'Please check connect!', buttons = ['OK'], default = 0)
-        #self.chart.create_text(15, self.pointmax[len(self.pointmin) - 1][0], text=self.content.weathers[i].mindegree, fill='red');
+
+      self.chart.create_line(15, self.pointmin[len(self.pointmin) - 1][0], 20, self.pointmin[len(self.pointmin) - 1][0], fill='blue')
+      self.chart.create_text(10, self.pointmin[len(self.pointmin) - 1][0], text=self.content.weathers[len(self.pointmin) - 1].mindegree, fill='blue');
 
   def updateLineChart(self):
       self.chart.delete(ALL)
@@ -175,13 +202,17 @@ class LineChart(Frame):
       self.drawLineChart()
 
 def GUI():
-  root = Tk()
-  content = Content(root)
-  chart = LineChart(root, content)
-  top = Top(root, content, chart)
+  root = Tk(className='MyWeather')
+  root.resizable(width = False, height = False)
 
-  top.pack()
-  content.pack()
-  chart.pack()
+  content = Content(root)
+  today = Today(root, content)
+  chart = LineChart(root, content)
+  top = Top(root, content, chart, today)
+
+  top.pack(fill=X)
+  today.pack(fill=X)
+  content.pack(fill=X)
+  chart.pack(fill=X)
 
   root.mainloop()
--- Weathers.py
+++ Weathers.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-import os.path
-import urllib
+# -*- coding: utf-8 -*-
 import urllib2
-import time
-import sys
-import BeautifulSoup
-import re
+import urllib
+import HTMLParser
+import json
+import os
+
+class MyParser(HTMLParser.HTMLParser):
+    def handle_starttag(self, tag, attr):
+        if tag == 'img':
+            #print attr[0][1]
+            urllib.urlretrieve(attr[0][1], 'Today.gif')
 
 class Weather():
-  def __init__(self, day = '  ', date = '  ', weather = '    ', maxdegree = '  ', mindegree = '  '):
-    self.day = str(day)
-    self.date = str(date).strip('日')
-    self.weather = str(weather)
-    self.maxdegree = str(maxdegree)
-    self.mindegree = str(mindegree)
-    self.message = self.day + '\t' + self.date + '日\t' + self.weather + (15 - (len(self.weather) / 3)) * ' ' + self.mindegree + '度 ~ ' + self.maxdegree + '度'
 
+  def __init__(self, d = {'day': ' ', 'date': '0 0 0', 'code': '3200', 'high': '32', 'low': '32', 'text': ' '}):
+     self.day = d['day']
+     self.date = d['date']
+     self.maxdegree = d['high']
+     self.mindegree = d['low']
+     self.text = d['text']
+     self.message = self.day + '\t' + self.date.split()[0]+ '\t' + self.text + (2 - (len(self.text) / 9)) * '\t' + self.mindegree + 'C~' + self.maxdegree + 'C'
 
 class Weathers(list):
     def __init__(self, iterable=[]):
         list.__init__(self, iterable)
-        for i in range(6):
+        for i in range(5):
             self.append(Weather())
 
-    #get Weather information from internet
-    #if there isn't Internet Connect, it will raise error
-     #if there is Internet Connect, function will return array of class Weather which get message from internet
     def setWeathersFromInternet(self):
 
         self.day = []
@@ -40,55 +40,37 @@ class Weathers(list):
         #define list to store maxdegree message
         self.mindegree = []
         #define list to store minidegree message
+        self.text = []
         weathers = []
         #define list to store array of class Weather object
 
-        fd = urllib2.urlopen(self.url, timeout = 10)
-        #download the page from url, and set timeout equal 5
+        result = urllib2.urlopen(self.url).read()
+        data = json.loads(result)
 
-        html = fd.read()
-        #html which is type of string includes the content of page
+        if data['query']['count'] == 0:
+            return 1
 
-        soup = BeautifulSoup.BeautifulSoup(html)
-        #build a class called BeatifulSoup, through this class to deal with HTML
-        content = soup.find(id = '7d')
-        #find the DOM structure whose id equal '7d'
-        #print soup.find(id='7d').prettify()
-        for daytag in content.findAll('h1'):
-          self.day.append(daytag.string)
-        #get all tag called 'h1', get the content between tags and add to the list of day
+        html =  dict(data['query']['results']['channel']['item'])['description']
 
-        for datetag in content.findAll('h2'):
-          self.date.append(datetag.string)
-        #get all tag called 'h2', get the content between tags and add to the list of date
+        parser = MyParser()
+        parser.feed(html)
+        #download the image from internet namely Today.gif
 
-        for weathertag in content.findAll('p', 'wea'):
-          self.weather.append(weathertag.string)
-        #get all tag called 'p' and di = 'wea', get the content between tags and add to the list of date
+        weathers = list(data['query']['results']['channel']['item']['forecast'])
 
-        sign = 0
-        for degree in content.findAll('span'):
-          if degree.string != None:
-            if sign == 0:
-              t1 = degree.string
-              sign = 1
-            elif sign == 1:
-              t2 = degree.string
-              sign = 0
-              if int(t1) > int(t2):
-                self.maxdegree.append(int(t1))
-                self.mindegree.append(int(t2))
-              else:
-                self.maxdegree.append(int(t2))
-                self.mindegree.append(int(t1))
-        #get the maxdegree and mindegree and store to array of mindegree and array of maxdegree
+        for i, wea in enumerate(weathers):
+            self.day.append(wea['day'])
+            wea['date'] = wea['date']
+            self.date.append(wea['date'])
+            self.weather.append(wea['code'])
+            wea['high'] = str((int(wea['high']) - 32) * 5 / 9)
+            wea['low'] = self[i].mindegree = str((int(wea['low']) - 32) * 5 / 9)
+            self.maxdegree.append(int(wea['high']))
+            self.mindegree.append(int(wea['low']))
+            self.text.append(wea['text'])
+            self[i] = Weather(wea)
 
-        for dayname, datename, weathername, maxdegreename, mindegreename in zip(self.day, self.date, self.weather, self.maxdegree, self.mindegree):
-          weathers.append(Weather(dayname, datename, weathername, maxdegreename, mindegreename))
-        #put all message store into the array of Weathers consisted by class Weather
-
-        for i, wea in enumerate(weathers[0:6]):
-            self[i] = wea
+        return 0
 
     def setDefaultCity(self):
       if os.path.isfile('data.txt'):
@@ -103,52 +85,24 @@ class Weathers(list):
       self.city = cityname
 
     def setURL(self):
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + self.city + "')"
+        yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
 
-      url = 'http://toy1.weather.com.cn/search?cityname=' + urllib.quote(self.city) + '&callback=jQuery182005500786968241289_1411741793130'
-      #encode the city name and add into the url
-
-      #define headers message as type dict namely date
-      data = {
-        'GET': 'url',
-        'Host':	'toy1.weather.com.cn',
-        'User-Agent': 	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',
-        'Accept':	'*/*',
-        'Accept-Language':	'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
-        'Accept-Encoding': 'gzip, deflate',
-        'Referer': 'http://www.weather.com.cn/',
-        'Connection': 'keep-alive'
-        }
-
-      request = urllib2.Request(url, headers = data)
-      #build the request
-      response = urllib2.urlopen(request, timeout=10).read()
-      #get the content of page as type of string
-      #the content include the code of the city you want to search
-
-      ss = re.findall('\"ref\":\"(.*?)\"', response)
-      #use re to get all code and store into list of ss
-
-      for item in ss:
-        if '~' + self.city + '~' in item:
-          code = item.split('~')[0]
-          break
-      #get the code of the city directly
-
-      self.url = 'http://www.weather.com.cn/weather1d/' + code + '.shtml'
+        self.url = yql_url
 
     #update data of the cache file
     #store the name of city into local filename namely 'data.txt'
     def saveWeathers(self):
-      file = open('data.txt', 'w')
-      file.write(self.city)
-      file.close()
+        file = open('data.txt', 'w')
+        file.write(self.city)
+        file.close()
 
 if __name__ == '__main__':
     weathers = Weathers()
-
+    weathers.setDefaultCity()
     weathers.setCity('Hefei')
     weathers.setURL()
-    print weathers.url
     weathers.setWeathersFromInternet()
 
     for wea in weathers:
